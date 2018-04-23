169f7f5f6eb533f29b7519d1e8ea0570e58bb0ce
306495389li <liguoshuai147@sina.com>
2018-04-23 22:09:43 +0800

    first commit

---
+++ shopping-api/src/main/java/com/electric/dubbo/registry/service/TestRegistryService.java
@@ -0,0 +1,9 @@
+package com.electric.dubbo.registry.service;
+
+/**
+ * @author guoshuai.li
+ * @date 2018-04-22 11:30:41
+ */
+public interface TestRegistryService {
+    public String hello(String name);
+}
